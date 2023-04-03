5dcc9893adf0dd95e7fe99cdecaf7d17f69f6281
Nulljy <74478749+Nulljy@users.noreply.github.com>
2023-04-04 00:32:28 +0900

    소인수분해
    https://school.programmers.co.kr/learn/courses/30/lessons/120852

---
+++ javaScript/lv0__while, Set.js
@@ -0,0 +1,44 @@
+
+console.log(solution(420)); // [2, 3, 5, 7]
+// 소인수분해
+//소인수분해란 어떤 수를 소수들의 곱으로 표현하는 것입니다. 예를 들어 12를 소인수 분해하면 2 * 2 * 3 으로 나타낼 수 있습니다. 따라서 12의 소인수는 2와 3입니다. 자연수 n이 매개변수로 주어질 때 n의 소인수를 오름차순으로 담은 배열을 return하도록 solution 함수를 완성해주세요.
+function app(n) {
+    let result = 1;
+    for(let i=2; i<=n; i++) {
+        if(n%i == 0) {
+            result = i;
+            break;
+        } else {
+            result = i;
+        }
+    }
+    return result;
+}
+
+function solution(n) {
+    let tmp = n;
+    let set = new Set();
+    while(tmp != 1) {
+        let ans = app(tmp);
+        set.add(ans);
+        tmp /= ans;
+    }
+    return [...set];
+}
+
+// 다른 풀이
+function solution(n) {
+    var answer = [];
+
+    for(let i = 2; i <= n; i++) {
+
+        while (n % i === 0) {
+
+            n = n / i;
+            answer.push(i);
+
+        }
+    }
+
+    return [...new Set(answer)];
+}
